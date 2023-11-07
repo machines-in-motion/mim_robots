ba0341d3809e1fefce77439db9d3d0a4d1bd4816
ameduri <avadesh02@gmail.com>
2023-11-07 16:36:41 -0500

    removed path modification in pinocchio demo

--- examples/pinocchio_demo.py
+++ examples/pinocchio_demo.py
@@ -1,8 +1,3 @@
-import pathlib
-import os
-python_path = pathlib.Path('.').absolute().parent/'python'
-os.sys.path.insert(1, str(python_path))
-
 from mim_robots.robot_loader import load_pinocchio_wrapper, get_robot_list
 
 print(get_robot_list())
