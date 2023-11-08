1b524784fa090292b875b7d4f87d331c6c9002bf
Sébastien Kleff <sk8001@nyu.edu>
2023-11-08 09:14:56 -0500

    test all robots in bullet

--- examples/bullet.py
+++ examples/bullet.py
@@ -6,16 +6,21 @@ from mim_robots.robot_list import MiM_Robots
 
 env = BulletEnvWithGround(p.GUI)
 # robot = load_bullet_wrapper("iiwa", controlled_joints =  ["A1", "A3", "A4", "A5",  "A7"])
+robot = load_bullet_wrapper("teststand")
 
-print(MiM_Robots.keys())
+print(robot)
+env.add_robot(robot)
 
-for rname in MiM_Robots.keys():
-    print("Adding "+rname)
-    robot = load_bullet_wrapper(rname)
-    env.add_robot(robot)
+# print(MiM_Robots.keys())
 
-# robot = load_bullet_wrapper("solo12")
-# print(robot)
+# for rname in MiM_Robots.keys():
+#     if(rname != 'teststand'):
+#         print("Adding "+rname)
+#         robot = load_bullet_wrapper(rname)
+#         env.add_robot(robot)
+
+# # robot = load_bullet_wrapper("solo12")
+# # print(robot)
 
 for i in range(100000):
     env.step()
