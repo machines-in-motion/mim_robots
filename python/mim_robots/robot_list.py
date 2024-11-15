f38ad3db965cd6fe901441072752268e70702533
Théo Martinez <tmartinezf@laas.fr>
2024-11-15 14:58:14 +0100

    reformatting

--- python/mim_robots/robot_list.py
+++ python/mim_robots/robot_list.py
@@ -2,22 +2,24 @@
 from dataclasses import dataclass
 import importlib.resources
 
+
 @dataclass
 class RobotInfo:
-    name : str
+    name: str
     nq: int
-    description : str
-    urdf_path : str
-    mesh_dir : str
-    xml_path : str
+    description: str
+    urdf_path: str
+    mesh_dir: str
+    xml_path: str
     dgm_path: str
-    fixed_base : bool
+    fixed_base: bool
+
 
 with importlib.resources.path(__package__, "robot_list.py") as p:
     package_path = p.parent.absolute()
 
 
-resources_path = str(package_path) + '/robots/'
+resources_path = str(package_path) + "/robots/"
 mesh_dir = resources_path
 
 # # TODO: MOVE THESE TO A YAML FILE
@@ -25,80 +27,84 @@ mesh_dir = resources_path
 
 
 MiM_Robots = {
-
-    "teststand" : RobotInfo(
-                    "teststand",
-                    2, \
-                    "2 Degree of Freedom Hopper", \
-                    resources_path + "teststand/urdf/teststand.urdf", \
-                    resources_path + "teststand", \
-                    resources_path + "teststand/xml/teststand.xml", \
-                    resources_path + "teststand/dgm_parameters_teststand.yaml", \
-                    True),
-    "iiwa" : RobotInfo(
-                    "iiwa", 
-                    7, \
-                    "Kuka IIWA LBR robot - 7 DoF", \
-                    resources_path + "kuka/urdf/iiwa.urdf", \
-                    resources_path + "kuka", \
-                    resources_path + "kuka/xml/iiwa.xml", \
-                    resources_path + "kuka/dgm_parameters_iiwa.yaml", \
-                    True),
-    "iiwa_ft_sensor_shell" : RobotInfo(
-                    "iiwa_ft_sensor_shell", 
-                    7, \
-                    "Kuka IIWA LBR robot - 7 DoF with the force torque sensor + plastic shell at the tip", \
-                    resources_path + "kuka/urdf/iiwa_ft_sensor_shell.urdf", \
-                    resources_path + "kuka", \
-                    resources_path + "kuka/xml/iiwa_ft_sensor_shell.xml", \
-                    resources_path + "kuka/dgm_parameters_iiwa.yaml", \
-                    True),
-    "iiwa_ft_sensor_ball" : RobotInfo(
-                    "iiwa_ft_sensor_ball", 
-                    7, \
-                    "Kuka IIWA LBR robot - 7 DoF with the force torque sensor shell at the tip with a ball", \
-                    resources_path + "kuka/urdf/iiwa_ft_sensor_ball.urdf", \
-                    resources_path + "kuka", \
-                    resources_path + "kuka/xml/iiwa_ft_sensor_ball.xml", \
-                    resources_path + "kuka/dgm_parameters_iiwa.yaml", \
-                    True),
-    "iiwa_gripper" : RobotInfo(
-                    "iiwa_gripper", 
-                    13, \
-                    "Kuka IIWA LBR robot - 7 DoF with gripper", \
-                    resources_path + "kuka/urdf/iiwa_gripper.urdf", \
-                    resources_path + "kuka", \
-                    resources_path + "kuka/xml/iiwa_gripper.xml", \
-                    None,
-                    True),
-    "iiwa_convex" : RobotInfo(
-                    "iiwa_convex", 
-                    7, \
-                    "Kuka IIWA LBR robot - 7 DoF with the collision model convex. Can be used to avoid collisions.", \
-                    resources_path + "kuka/urdf/iiwa_convex.urdf", \
-                    resources_path + "kuka", \
-                    resources_path + "kuka/xml/iiwa.xml", \
-                    resources_path + "kuka/dgm_parameters_iiwa.yaml", \
-                    True),
-    "iiwa_convex_ft_sensor_shell" : RobotInfo(
-                    "iiwa_convex_ft_sensor_shell", 
-                    7, \
-                    "Kuka IIWA LBR robot - 7 DoF with the collision model convex and ft sensor + plastic shell at the tip. Can be used to avoid collisions.", \
-                    resources_path + "kuka/urdf/iiwa_convex_ft_sensor_shell.urdf", \
-                    resources_path + "kuka", \
-                    resources_path + "kuka/xml/iiwa_ft_sensor_shell.xml", \
-                    resources_path + "kuka/dgm_parameters_iiwa.yaml", \
-                    True),
-    "solo12" : RobotInfo(
-                    "solo12",
-                    12,
-                    "12 Degree of freedom quadruped robot", \
-                    resources_path + "solo12/urdf/solo12.urdf", \
-                    resources_path + "solo12", \
-                    resources_path + "solo12/xml/solo12.xml", \
-                    resources_path + "solo12/dgm_parameters_solo12_nyu.yaml", \
-                    False),
+    "teststand": RobotInfo(
+        "teststand",
+        2,
+        "2 Degree of Freedom Hopper",
+        resources_path + "teststand/urdf/teststand.urdf",
+        resources_path + "teststand",
+        resources_path + "teststand/xml/teststand.xml",
+        resources_path + "teststand/dgm_parameters_teststand.yaml",
+        True,
+    ),
+    "iiwa": RobotInfo(
+        "iiwa",
+        7,
+        "Kuka IIWA LBR robot - 7 DoF",
+        resources_path + "kuka/urdf/iiwa.urdf",
+        resources_path + "kuka",
+        resources_path + "kuka/xml/iiwa.xml",
+        resources_path + "kuka/dgm_parameters_iiwa.yaml",
+        True,
+    ),
+    "iiwa_ft_sensor_shell": RobotInfo(
+        "iiwa_ft_sensor_shell",
+        7,
+        "Kuka IIWA LBR robot - 7 DoF with the force torque sensor + plastic shell at the tip",
+        resources_path + "kuka/urdf/iiwa_ft_sensor_shell.urdf",
+        resources_path + "kuka",
+        resources_path + "kuka/xml/iiwa_ft_sensor_shell.xml",
+        resources_path + "kuka/dgm_parameters_iiwa.yaml",
+        True,
+    ),
+    "iiwa_ft_sensor_ball": RobotInfo(
+        "iiwa_ft_sensor_ball",
+        7,
+        "Kuka IIWA LBR robot - 7 DoF with the force torque sensor shell at the tip with a ball",
+        resources_path + "kuka/urdf/iiwa_ft_sensor_ball.urdf",
+        resources_path + "kuka",
+        resources_path + "kuka/xml/iiwa_ft_sensor_ball.xml",
+        resources_path + "kuka/dgm_parameters_iiwa.yaml",
+        True,
+    ),
+    "iiwa_gripper": RobotInfo(
+        "iiwa_gripper",
+        13,
+        "Kuka IIWA LBR robot - 7 DoF with gripper",
+        resources_path + "kuka/urdf/iiwa_gripper.urdf",
+        resources_path + "kuka",
+        resources_path + "kuka/xml/iiwa_gripper.xml",
+        None,
+        True,
+    ),
+    "iiwa_convex": RobotInfo(
+        "iiwa_convex",
+        7,
+        "Kuka IIWA LBR robot - 7 DoF with the collision model convex. Can be used to avoid collisions.",
+        resources_path + "kuka/urdf/iiwa_convex.urdf",
+        resources_path + "kuka",
+        resources_path + "kuka/xml/iiwa.xml",
+        resources_path + "kuka/dgm_parameters_iiwa.yaml",
+        True,
+    ),
+    "iiwa_convex_ft_sensor_shell": RobotInfo(
+        "iiwa_convex_ft_sensor_shell",
+        7,
+        "Kuka IIWA LBR robot - 7 DoF with the collision model convex and ft sensor + plastic shell at the tip. Can be used to avoid collisions.",
+        resources_path + "kuka/urdf/iiwa_convex_ft_sensor_shell.urdf",
+        resources_path + "kuka",
+        resources_path + "kuka/xml/iiwa_ft_sensor_shell.xml",
+        resources_path + "kuka/dgm_parameters_iiwa.yaml",
+        True,
+    ),
+    "solo12": RobotInfo(
+        "solo12",
+        12,
+        "12 Degree of freedom quadruped robot",
+        resources_path + "solo12/urdf/solo12.urdf",
+        resources_path + "solo12",
+        resources_path + "solo12/xml/solo12.xml",
+        resources_path + "solo12/dgm_parameters_solo12_nyu.yaml",
+        False,
+    ),
 }
-
-
-
--- python/mim_robots/robot_loader.py
+++ python/mim_robots/robot_loader.py
@@ -1,19 +1,21 @@
-## This class wraps around robot_description to allow use of MIM XML files 
-## AKA - Tri fingers, kuka iiwa and Solo12. 
+## This class wraps around robot_description to allow use of MIM XML files
+## AKA - Tri fingers, kuka iiwa and Solo12.
 ## Author : Avadesh Meduri
 ## Date : 29/11/2022
 
-from . robot_list import MiM_Robots
+from .robot_list import MiM_Robots
 import numpy as np
 
 try:
     from robot_descriptions.loaders.pinocchio import load_robot_description
-except Exception as e: print(e)
+except Exception as e:
+    print(e)
+
 
 def get_robot_list():
     """
-        Returns the list of robots available
-    """    
+    Returns the list of robots available
+    """
     print("The list of available robots are : \n")
     for robot_name in MiM_Robots.keys():
         print(robot_name + ": " + MiM_Robots[robot_name].description)
@@ -21,57 +23,71 @@ def get_robot_list():
 
 def load_mujoco_model(robot_name: str):
     """
-        Returns mujoco model
-        Input:
-            robot_name : the name of the robot you want to load
-    """ 
+    Returns mujoco model
+    Input:
+        robot_name : the name of the robot you want to load
+    """
     try:
         import mujoco
         from robot_descriptions.loaders.mujoco import load_robot_description
-    except Exception as e: print(e)
-    
+    except Exception as e:
+        print(e)
+
     try:
         RobotInfo = MiM_Robots[robot_name]
         try:
             return mujoco.MjModel.from_xml_path(RobotInfo.xml_path)
-        except Exception as e: print(e)    
+        except Exception as e:
+            print(e)
     except:
         try:
             mj_name = robot_name + "_mj_description"
             return load_robot_description(mj_name)
-        except Exception as e: print("No ROBOT", e)
+        except Exception as e:
+            print("No ROBOT", e)
 
 
-def load_bullet_wrapper(robot_name, locked_joints = None):
+def load_bullet_wrapper(robot_name, locked_joints=None):
     try:
         import pybullet
         from robot_descriptions.loaders.pybullet import load_robot_description
-    except Exception as e: print(e)
+    except Exception as e:
+        print(e)
 
     try:
-        if(robot_name == 'iiwa'):
+        if robot_name == "iiwa":
             from mim_robots.robots.kuka.pinbullet.iiwaWrapper import IiwaRobot
+
             return IiwaRobot(MiM_Robots["iiwa"], locked_joints)
-        elif(robot_name == 'iiwa_convex'):
+        elif robot_name == "iiwa_convex":
             from mim_robots.robots.kuka.pinbullet.iiwaWrapper import IiwaRobot
+
             return IiwaRobot(MiM_Robots["iiwa_convex"], locked_joints)
-        elif(robot_name == 'iiwa_ft_sensor_shell'):
+        elif robot_name == "iiwa_ft_sensor_shell":
             from mim_robots.robots.kuka.pinbullet.iiwaWrapper import IiwaRobot
+
             return IiwaRobot(MiM_Robots["iiwa_ft_sensor_shell"], locked_joints)
-        elif(robot_name == 'iiwa_convex_ft_sensor_shell'):
+        elif robot_name == "iiwa_convex_ft_sensor_shell":
             from mim_robots.robots.kuka.pinbullet.iiwaWrapper import IiwaRobot
+
             return IiwaRobot(MiM_Robots["iiwa_convex_ft_sensor_shell"], locked_joints)
-        elif(robot_name == 'iiwa_ft_sensor_ball'):
+        elif robot_name == "iiwa_ft_sensor_ball":
             from mim_robots.robots.kuka.pinbullet.iiwaWrapper import IiwaRobot
+
             return IiwaRobot(MiM_Robots["iiwa_ft_sensor_ball"], locked_joints)
-        elif(robot_name == 'iiwa_gripper'):
+        elif robot_name == "iiwa_gripper":
             from mim_robots.robots.kuka.pinbullet.iiwaWrapper import IiwaRobot
+
             return IiwaRobot(MiM_Robots["iiwa_gripper"], locked_joints)
-        elif(robot_name == 'teststand'):
-            from mim_robots.robots.teststand.pinbullet.teststand_wrapper import TeststandRobot
+        elif robot_name == "teststand":
+            from mim_robots.robots.teststand.pinbullet.teststand_wrapper import (
+                TeststandRobot,
+            )
+
             robot = TeststandRobot(MiM_Robots["teststand"])
-        elif(robot_name == 'solo12'):
+        elif robot_name == "solo12":
             from mim_robots.robots.solo12.pinbullet.solo12wrapper import Solo12Robot
+
             robot = Solo12Robot(MiM_Robots["solo12"], locked_joints)
             return robot
         else:
@@ -83,7 +99,8 @@ def load_bullet_wrapper(robot_name, locked_joints = None):
         except:
             print("Robot description not support for pybullet")
 
-def load_pinocchio_wrapper(robot_name: str, locked_joints = None):
+
+def load_pinocchio_wrapper(robot_name: str, locked_joints=None):
     """
     Returns pinocchio wrapper model
     Input:
@@ -91,39 +108,43 @@ def load_pinocchio_wrapper(robot_name: str, locked_joints = None):
     """
     try:
         import pinocchio as pin
-    except Exception as e: print(e)
+    except Exception as e:
+        print(e)
 
     try:
         RobotInfo = MiM_Robots[robot_name]
         try:
             if RobotInfo.fixed_base:
                 full_robot = pin.RobotWrapper.BuildFromURDF(
-                                            filename=RobotInfo.urdf_path,
-                                            package_dirs=RobotInfo.mesh_dir,
-                                            root_joint=None,
-                                            )
+                    filename=RobotInfo.urdf_path,
+                    package_dirs=RobotInfo.mesh_dir,
+                    root_joint=None,
+                )
             else:
                 full_robot = pin.RobotWrapper.BuildFromURDF(
-                                            RobotInfo.urdf_path,
-                                            RobotInfo.mesh_dir,
-                                            pin.JointModelFreeFlyer()
-                                            )
-            if(locked_joints is None):
+                    RobotInfo.urdf_path, RobotInfo.mesh_dir, pin.JointModelFreeFlyer()
+                )
+            if locked_joints is None:
                 return full_robot
             else:
                 locked_joints_ids = []
                 for joint_name in locked_joints:
                     locked_joints_ids.append(full_robot.model.getJointId(joint_name))
-                reduced_model, [visual_model, collision_model] = pin.buildReducedModel(full_robot.model, [full_robot.visual_model, full_robot.collision_model],  locked_joints_ids, np.zeros(RobotInfo.nq))   
-                return pin.robot_wrapper.RobotWrapper(reduced_model, collision_model, visual_model)  
-        except Exception as e: print("ERROR in URDF:", e)
+                reduced_model, [visual_model, collision_model] = pin.buildReducedModel(
+                    full_robot.model,
+                    [full_robot.visual_model, full_robot.collision_model],
+                    locked_joints_ids,
+                    np.zeros(RobotInfo.nq),
+                )
+                return pin.robot_wrapper.RobotWrapper(
+                    reduced_model, collision_model, visual_model
+                )
+        except Exception as e:
+            print("ERROR in URDF:", e)
     except:
-        try:  
+        try:
             pin_name = robot_name + "_description"
             print(pin_name)
             return load_robot_description(pin_name)
-        except Exception as e: print("No ROBOT", e)
-    
-
-
-        
+        except Exception as e:
+            print("No ROBOT", e)
