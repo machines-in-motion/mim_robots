09abe3b5aaab3b489a49260a96e0d74ae3de9314
Théo Martinez <tmartinezf@laas.fr>
2024-11-20 09:22:15 +0000

    add possibility to load panda wrapper

--- python/mim_robots/robot_list.py
+++ python/mim_robots/robot_list.py
@@ -98,6 +98,16 @@ MiM_Robots = {
                     resources_path + "solo12/xml/solo12.xml", \
                     resources_path + "solo12/dgm_parameters_solo12_nyu.yaml", \
                     False),
+    "panda": RobotInfo(
+                    "panda",
+                    7,
+                    "Franka Emika Panda robot - 7 DoF with the collision model convex and ft sensor + plastic shell at the tip. Can be used to avoid collisions.",
+                    resources_path + "panda/urdf/model.urdf",
+                    resources_path + "panda",
+                    resources_path + "panda/xml/iiwa.xml",
+                    resources_path + "panda/dgm_parameters_iiwa.yaml",
+                    True,
+                    ),
 }
 
 
